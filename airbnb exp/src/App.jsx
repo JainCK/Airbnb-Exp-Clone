8160052ca1b82e7bc1d58b7bdd26a5f6185a5c65
zippyzag88 <kuriakosejain456@gmail.com>
2023-08-31 16:13:55 +0530

    card comp and data file

--- airbnb exp/src/App.jsx
+++ airbnb exp/src/App.jsx
@@ -2,12 +2,28 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Card from './components/Card'
+import data from './components/data'
 
 function App() {
+
+  const cards = data.map(item => {
+    return (
+      <Card 
+        img={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        location={item.location}
+        title={item.title}
+        price={item.price}
+      />
+      )
+  })
   return(
     <>
     <Navbar />
     <Hero />
+    {cards}
     </>
   )
 }
